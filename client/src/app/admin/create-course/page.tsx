b9a58d0c491e69ae65b26001ab88def790540124
arpitsahuu <arpitsahu358@gmail.com>
2024-06-13 16:31:00 +0530

    Add create coures form

---
+++ client/src/app/admin/create-course/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React from 'react'
+import AdminSidebar from "@/components/Admin/sidebar/AdminSidebar";
+import CreateCourse from "../../../components/Admin/Course/CreateCourse";
+import DashboardHeader from '../../../components/Admin/DashboardHeader';
+import Heading from '@/components/utils/Heading';
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+        <Heading
+         title="Lernify - Admin"
+         description="ELearning is a platform for students to learn and get help from teachers"
+         keywords="Prograaming,MERN,Redux,Machine Learning"
+        />
+        <div className="flex">
+            <div className="1500px:w-[16%] w-1/5">
+                <AdminSidebar />
+            </div>
+            <div className="w-[85%]">
+               <DashboardHeader />
+               <CreateCourse /> 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
