a769ca12d08a8960533ba3e444b7ff66da6d656d
arpitsahuu <arpitsahu358@gmail.com>
2024-07-01 13:38:28 +0530

    Modify layout modul

---
+++ server/src/controllers/layoutController.ts
@@ -0,0 +1,71 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsyncError } from "../middlewares/catchAsyncError";
+import errorHandler from "../utils/errorHandler";
+import axios from "axios";
+import { redis } from "../models/redis";
+
+
+// GENERATE THE VIDEOURL
+export const generateVideoUrl = catchAsyncError(
+    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { type } = req.body;
+            const isTypeExist = await LayoutModel.findOne({ type });
+            if (isTypeExist) {
+              return next(new errorHandler(`${type} already exist`, 400));
+            }
+            if (type === "Banner") {
+              const { image, title, subTitle } = req.body;
+              const myCloud = await cloudinary.v2.uploader.upload(image, {
+                folder: "layout",
+              });
+              const banner = {
+                type: "Banner",
+                banner: {
+                  image: {
+                    public_id: myCloud.public_id,
+                    url: myCloud.secure_url,
+                  },
+                  title,
+                  subTitle,
+                },
+              };
+              await LayoutModel.create(banner);
+            }
+            if (type === "FAQ") {
+              const { faq } = req.body;
+              const faqItems = await Promise.all(
+                faq.map(async (item: any) => {
+                  return {
+                    question: item.question,
+                    answer: item.answer,
+                  };
+                })
+              );
+              await LayoutModel.create({ type: "FAQ", faq: faqItems });
+            }
+            if (type === "Categories") {
+              const { categories } = req.body;
+              const categoriesItems = await Promise.all(
+                categories.map(async (item: any) => {
+                  return {
+                    title: item.title,
+                  };
+                })
+              );
+              await LayoutModel.create({
+                type: "Categories",
+                categories: categoriesItems,
+              });
+            }
+      
+            res.status(200).json({
+              success: true,
+              message: "Layout created successfully",
+            });
+          } catch (error: any) {
+            return next(new ErrorHandler(error.message, 500));
+          }
+    }
+  );
+
---
+++ server/src/models/layout.model.ts
@@ -0,0 +1,39 @@
+import {Schema,model,Document} from "mongoose";
+
+export interface FaqItem extends Document{
+    question: string;
+    answer: string;
+}
+
+export interface Category extends Document{
+    title:string;
+}
+
+
+interface Layout extends Document{
+    type: string;
+    faq: FaqItem[];
+    categories: Category[];
+}
+
+const faqSchema = new Schema<FaqItem> ({
+    question: {type: String},
+    answer: {type: String},
+});
+
+const categorySchema = new Schema<Category> ({
+    title: {type:String},
+});
+
+
+
+
+const layoutSchema = new Schema<Layout>({
+   type:{type:String},
+   faq: [faqSchema],
+   categories: [categorySchema],
+});
+
+const LayoutModel = model<Layout>('Layout',layoutSchema);
+
+export default LayoutModel;
